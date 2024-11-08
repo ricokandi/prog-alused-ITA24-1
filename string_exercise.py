9c60087a1648dcb43340aa81ee824c15aca29d1e
Rico Kandi <rico.kandi@voco.ee>
2024-11-08 09:33:41 +0200

    tehtud

---
+++ string_exercise.py
@@ -0,0 +1,18 @@
+# Write your code here
+first_name = "James"
+last_name = "Bond"
+
+full_name = first_name + " " + last_name
+
+self_description_sentence = f"My name is {last_name}, {full_name}."
+
+
+cake = "vahukoor\nmarjad\ntäidis\npõhi"
+print(cake)
+
+
+original_string = "Programming is fun!"
+
+backwards = original_string[::-1]
+every_other = original_string[::2]
+first_word_reversed = original_string[10::-1]
